0c4812882a262d2d110143c62788669b8397f8c2
Lu, Bin <blu@wm.com>
2022-05-18 09:29:38 -0500

    add comp

--- angular-app/src/app/receipes/receipe-list/receipe-list.component.ts
+++ angular-app/src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Receipe } from '../receipe.model';
 
 @Component({
   selector: 'app-receipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceipeListComponent implements OnInit {
 
+  receipes:Receipe[]=[
+    new Receipe('Sample Receipe','This is a sample receipe for test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'),
+    new Receipe('Sample Receipe 2','This is a sample receipe 2 for test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341')
+  ]
   constructor() { }
 
   ngOnInit(): void {
