4cd6bb3482812ef98b21d95727f77deba13b580d
Lu, Bin <blu@wm.com>
2022-05-20 00:28:09 -0500

    Add binding

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{"type":"server","name":"Test server","content":"This is a test server"}];
+
+  onServerCreated(eventData:{"name":string,"content":string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: eventData.name,
+      content: eventData.content
+    });
+  }
+
+  onBlueprintCreated(eventData:{"name":string,"content":string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: eventData.name,
+      content: eventData.content
+    });
+  }
+
 }
