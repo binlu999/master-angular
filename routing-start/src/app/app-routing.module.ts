b17dd1224e59a2f1b85d8a3eabda7bf58d45ea63
Lu, Bin <blu@wm.com>
2022-05-31 00:22:26 -0500

    Deactivated

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth-gurad.service";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -10,8 +12,14 @@ import { UsersComponent } from "./users/users.component";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'servers', component: ServersComponent, children:[
-      { path: ':id/edit',component:EditServerComponent},
+    { path: 'servers', 
+      //canActivate:[AuthGuard], 
+      canActivateChild:[AuthGuard],
+      component: ServersComponent, children:[
+      { path: ':id/edit',
+        component:EditServerComponent,
+        canDeactivate:[CanDeactivateGuard]
+      },
       { path: ':id',component:ServerComponent}
     ] },
     { path: 'users', component: UsersComponent, children:[
--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
@@ -32,6 +34,16 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+  canDeactivate(): Observable<boolean>|Promise<boolean>| boolean {
+    if(!this.allowEdit){
+      return true;
+    }
+    if(this.serverName!==this.server.name || this.serverStatus!==this.server.status)
+    return true;
+
+    return true;
   }
 
 }
