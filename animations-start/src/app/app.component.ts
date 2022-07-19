e17313637e10106be6871663b26c9df0903e1440
Lu, Bin <blu@wm.com>
2022-07-19 12:16:41 -0500

    pwa

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,54 +1,140 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations:[
+  animations: [
     trigger('divState', [
-      state('normal',style({
-        'background-color':'red',
+      state('normal', style({
+        'background-color': 'red',
         transform: 'translateX(0)'
       })),
-      state('highlighted',style({
-        'background-color':'blue',
+      state('highlighted', style({
+        'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
-      transition('normal <=> highlighted',animate(300))
+      transition('normal <=> highlighted', animate(300))
     ]),
     trigger('wildState', [
-      state('normal',style({
-        'background-color':'red',
+      state('normal', style({
+        'background-color': 'red',
         transform: 'translateX(0)'
       })),
-      state('highlighted',style({
-        'background-color':'blue',
+      state('highlighted', style({
+        'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
-      state('shrunken',style({
-        'background-color':'green',
-        transform:'translateX(0) scale(0.5)'
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
       })),
-      transition('normal => highlighted',animate(300)),
+      transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
-      transition('shrunken<=>*', animate(500))
-    ])
+      transition('shrunken1<=>*', animate(500, style({
+        borderRadius: '50px'
+      }))),
+      transition('shrunken<=>*', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(500)]),
+      transition('* => void', [
+        animate(500, style({
+          transform: 'translateX(300px)',
+          opacity: 0
+        }))
+      ]),
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          }),
+        ]))
+      ]),
+      transition('*=>void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
+      ])
+    ]),
   ]
 })
 export class AppComponent {
-  state='normal';
-  wildState='normal';
+  state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-  onAnimate(){
-    this.state === 'normal'? this.state='highlighted':this.state='normal';
-    this.wildState === 'normal'? this.wildState='highlighted':this.wildState='normal';
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
 
-  onShrink(){
-    this.wildState='shrunken';
+  onShrink() {
+    this.wildState = 'shrunken';
   }
-    onAdd(item) {
-      this.list.push(item);
+  onAdd(item) {
+    this.list.push(item);
+  }
+  onDelete(item) {
+    console.log('Item is' + item);
+    const index = this.list.indexOf(item);
+    console.log('Item index ' + index);
+    if (index > -1) {
+      this.list.splice(index, 1);
     }
+  }
+
+  onAnimateStart(event){
+    console.log(event);
+  }
+
+  onAnimateEnd(event){
+    console.log(event);
+  }
 }
